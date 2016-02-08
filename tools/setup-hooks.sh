b09b73b42989305b7b3ab705cefccadafd897437
Fabian Köhler <fkoehler1024@googlemail.com>
2016-02-08 22:11:48 +0100

    change git hook

--- tools/setup-hooks.sh
+++ tools/setup-hooks.sh
@@ -18,9 +18,9 @@ else
 # Post-{commit,checkout,merge} hook for the gitinfo2 package
 #
 # Get the first tag found in the history from the current HEAD
-FIRSTTAG=\$(git describe --tags --always --dirty='-*' 2>/dev/null)
+FIRSTTAG=\$(git describe --tags --always --dirty='-dirty' 2>/dev/null)
 # Get the first tag in history that looks like a Release
-RELTAG=\$(git describe --tags --long --always --dirty='-*' --match '[0-9]*.*' 2>/dev/null)
+RELTAG=\$(git describe --tags --long --always --dirty='-dirty' --match '[0-9]*.*' 2>/dev/null)
 # Hoover up the metadata
 git --no-pager log -1 --date=short --decorate=short \
     --pretty=format:"\usepackage[%
